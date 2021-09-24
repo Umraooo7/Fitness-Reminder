96de4eb9aefd9643d20610d18723d0ef7c36f4d2
ABHISHEK KUMAR <aku.kiet@gmail.com>
2021-09-25 00:20:30 +0530

    recursive file
    Without break

--- Fitness-Alarm.py
+++ Fitness-Alarm.py
@@ -36,89 +36,93 @@ def log_eye():
       )
 
 # log_eye()
+def alarm():
+    """recursive function""" 
+    c=0
+    while((hr() >= 16) and (hr() <= 24)):
+        log_eye()
+        print("hour: ",hr()," minute: ", mint())       
+        l = 3500
+ 
 
-c=0
-# while((hr() >= 16) and (hr() <= 24)):
-while((hr() == 0)):
-    log_eye()
-    print("hour: ",hr()," minute: ", mint())
-    l = 3500
-#     print(l-c)
+        if ((mint() == 30) 
+            or (mint() == 15) 
+            or (mint() == 0) 
+            or (mint() == 45)):
+            """for drinking water"""
 
-    if ((mint() == 30)
-        or (mint() == 15)
-        or (mint() == 5)
-        or (mint() == 45)):
-        """for drinking water"""
+            pygame.mixer.music.load('water.mp3')
+            pygame.mixer.music.play(-1)
 
-        pygame.mixer.music.load('water.mp3')
-        pygame.mixer.music.play(-1)
+            print("________Time to drink Water_____")
+            usr = (input("Type drank:")).lower()
 
-        print("________Time to drink Water_____")
-        usr = (input("Type drank:")).lower()
+            if usr == "drank":
+                a = input("How much you drank in ml: ")
+                c = c + int(a)
+                if int(a) > 0:
+                    print(l-c)
+                    with open ("LogFile.txt",'a+') as f:
+                        f.write(str(getdate()) + ': Water drank: ' + a +' ml and ' 
+                                + str(l - int(c))  + " ml water left\n")
+                    with open ("LogFile.txt") as f:
+                        print(f.readlines())
+                    pygame.mixer.music.stop()
 
-        if usr == "drank":
-            a = input("How much you drank in ml: ")
-            c = c + int(a)
-            if int(a) > 0:
-                print(l-c)
+
+            else:
+                print("just type drank")
+
+        if ((mint() == 30) 
+              or (mint() == 0)):
+            """For pyhsical workout"""
+
+            pygame.mixer.music.load('pyhsical.mp3')
+            pygame.mixer.music.play(-1)
+
+            print("________Time to do Physical Workout_____")
+            usr = (input("Type done:")).lower()
+
+            if usr == "done":
+                a = input("Press 1: After Completing Workout-")
                 with open ("LogFile.txt",'a+') as f:
-                    f.write(str(getdate()) + ': Water drank: ' + a +' ml and '
-                            + str(l - int(c))  + " ml water left\n")
+                    f.write(str(getdate()) + ': Workout done\n')
                 with open ("LogFile.txt") as f:
                     print(f.readlines())
-                pygame.mixer.music.stop()
-                break
+                if (int(a) > 0):
+                    pygame.mixer.music.stop()
+ 
+            else:
+                print("just type done")
+
+        if (log_eye()):
+            """For eye workout"""
+
+            pygame.mixer.music.load('eyes.mp3')
+            pygame.mixer.music.play(-1)
+
+            print("________Time to do Eye excercise_____")
+            usr = (input("Type done:")).lower()
+
+            if usr == "done":
+                a = input("Press 1: For Completing Workout")
+                with open ("LogFile.txt",'a+') as f:
+                    f.write(str(getdate()) + ': Eye excercise done\n')
+                with open ("LogFile.txt") as f:
+                    print(f.readlines())
+                if (int(a) > 0):
+                    pygame.mixer.music.stop()
+
+            else:
+                print("just type done")
 
         else:
-            print("just type drank")
+            print("Breakkk...")
+            break
+            alarm()
 
-    if ((mint() == 30)
-          or (mint() == 0)):
-        """For pyhsical workout"""
+            
+# There is a problem if some one drink water within a minute, it will keep running
 
-        pygame.mixer.music.load('pyhsical.mp3')
-        pygame.mixer.music.play(-1)
+alarm()
 
-        print("________Time to do Physical Workout_____")
-        usr = (input("Type done:")).lower()
-
-        if usr == "done":
-            a = input("Press 1: After Completing Workout-")
-            with open ("LogFile.txt",'a+') as f:
-                f.write(str(getdate()) + ': Workout done\n')
-            with open ("LogFile.txt") as f:
-                print(f.readlines())
-            if (int(a) > 0):
-                pygame.mixer.music.stop()
-                break
-        else:
-            print("just type done")
-
-    if (log_eye()):
-        """For eye workout"""
-
-        pygame.mixer.music.load('eyes.mp3')
-        pygame.mixer.music.play(-1)
-
-        print("________Time to do Eye excercise_____")
-        usr = (input("Type done:")).lower()
-
-        if usr == "done":
-            a = input("Press 1: For Completing Workout")
-            with open ("LogFile.txt",'a+') as f:
-                f.write(str(getdate()) + ': Eye excercise done\n')
-            with open ("LogFile.txt") as f:
-                print(f.readlines())
-            if (int(a) > 0):
-                pygame.mixer.music.stop()
-                break
-        else:
-            print("just type done")
-
-    else:
-        print("Breakkk...")
-        break
-#To remove all the break function, loop will run endlessly
-#Challenge is to do with recurssive loop.
-#Recursion possible
